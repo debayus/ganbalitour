02e541bf01bbdc75b1ccc652c69a9c364831e23d
Gede Bayu <bayusth@gmail.com>
2024-02-19 18:12:56 +0800

    update

--- js/tour.js
+++ js/tour.js
@@ -1,4 +1,35 @@
 const tourPage = () => {
+  $.getJSON("/data/tour.json", (data) => {
+    var html = "";
+    var keys = Object.keys(data);
+    keys.forEach((key) => {
+      const url = `/tour.html?id=${key}`;
+      html += `
+      <article class="tg-post tg-verticaltop">
+        <figure>
+          <a href="${url}"
+            ><img src="images/blog/img-${key}.jpg" alt="image description"
+          /></a>
+        </figure>
+        <div class="tg-postcontent">
+          </div>
+          <div class="tg-posttitle">
+            <h2>
+              <a href="${url}"
+                >${data[key]["nama"]}</a
+              >
+            </h2>
+          </div>
+          <a class="tg-btnreadmore" href="${url}">Read More</a>
+        </div>
+      </article>
+      `;
+    });
+    $(".page-tour").html(html);
+  });
+};
+
+const tourDetailPage = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const id = urlParams.get("id");
   $.getJSON("/data/tour.json", (data) => {
@@ -14,5 +45,9 @@ const tourPage = () => {
 };
 
 if (window.location.pathname === "/tour.html") {
+  tourDetailPage();
+}
+
+if (window.location.pathname === "/driverpackagetour.html") {
   tourPage();
 }
